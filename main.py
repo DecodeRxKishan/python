751f93dfd5775190acb54bb5afa892d6f33bd588
Kishan godhia <92265501+DecodeRxKishan@users.noreply.github.com>
2023-01-10 22:28:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ Item_name=input("Enter the name of the item: ")
 Quantity=int(input("Enter the Quantity of the item: "))
 price=float(input("Enter the price of the item: "))
 total=(Quantity * price)
-
 print("##################BILL####################")
 print("Item name     Quantity     price")
 print(" ", Item_name,"      ", Quantity,"       ", price)
